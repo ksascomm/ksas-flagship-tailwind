c1f30c6aa5dc97c6e4948b483d79e8c6b3b68f7b
ksascomm <ksascomm@gmail.com>
2021-07-22 11:27:34 -0400

    Fix bug of duplicate event

--- template-parts/front-page/find-program.php
+++ template-parts/front-page/find-program.php
@@ -75,7 +75,7 @@
 		</div>
 		<!-- Graduate Full-Time-->
 		<div class="front-studyfield-card group">
-			<a href="academics/fields/#filter=.full_time_program" onclick="ga('send', 'event', 'Fields', 'Homepage', 'Graduate PT');">
+			<a href="academics/fields/#filter=.full_time_program" onclick="ga('send', 'event', 'Fields', 'Homepage', 'Graduate FT');">
 				<svg class="absolute bottom-0 left-0 mb-8" viewBox="0 0 375 283" fill="none" style="transform: scale(1.5); opacity: 0.1;">
 					<rect x="159.52" y="175" width="152" height="152" rx="8" transform="rotate(-45 159.52 175)" fill="white"/>
 					<rect y="107.48" width="152" height="152" rx="8" transform="rotate(-45 0 107.48)" fill="white"/>
