7a5c27bca76fffe16a3ef638f91303b27d0e19e8
ksascomm <ksasweb@jhu.edu>
2024-03-20 14:19:44 -0400

    Update twitter icon

--- template-parts/front-page/social-wall.php
+++ template-parts/front-page/social-wall.php
@@ -14,7 +14,7 @@
 			<h1 class="sm:text-3xl text-2xl font-serif lg:w-1/3 lg:mb-0 mt-2">Connect with Us</h1>
 			<div class="flex flex-wrap flex-row md:flex-row-reverse lg:pl-6 lg:w-2/3 mx-auto leading-relaxed  font-heavy font-bold pb-4">
 				<a href="https://facebook.com/JHUArtsSciences" class="py-3 px-4 text-blue hover:text-primary"><span class="fa-brands fa-facebook fa-2x"></span><span class="screen-reader-text">Facebook</span></a>
-				<a href="https://twitter.com/JHUArtsSciences" class="py-3 px-4 text-blue hover:text-primary"><span class="fa-brands fa-twitter fa-2x"></span><span class="screen-reader-text">Twitter</span></a>
+				<a href="https://twitter.com/JHUArtsSciences" class="py-3 px-4 text-blue hover:text-primary"><span class="fa-brands fa-x-twitter fa-2x"></span><span class="screen-reader-text">Twitter</span></a>
 				<a href="https://www.youtube.com/user/jhuksas" class="py-3 px-4 text-blue hover:text-primary"><span class="fa-brands fa-youtube fa-2x"></span><span class="screen-reader-text">YouTube</span></a>
 				<a href="https://www.tiktok.com/@jhuartssciences" class="py-3 px-4 text-blue hover:text-primary"><span class="fa-brands fa-tiktok fa-2x"></span><span class="screen-reader-text">TikTok</span></a>
 				<a href="https://www.instagram.com/JHUArtsSciences/" class="py-3 px-4 text-blue hover:text-primary"><span class="fa-brands fa-instagram fa-2x"></span><span class="screen-reader-text">Instagram</span></a>
