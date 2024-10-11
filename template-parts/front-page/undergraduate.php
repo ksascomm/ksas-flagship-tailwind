f957b643ec997ce37e94feb386f4dee74d96c3cf
ksascomm <ksasweb@jhu.edu>
2024-10-11 10:17:43 -0400

    Update Undergraduate Experience section

--- template-parts/front-page/undergraduate.php
+++ template-parts/front-page/undergraduate.php
@@ -13,10 +13,10 @@
 		<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile" style="grid-template-columns:auto 40%">
 			<div class="wp-block-media-text__content">
 				<h1 class="text-4xl leading-snug">The Undergraduate <span class="text-blue">Experience</span></h1>
-				<p class="text-2xl py-4">The undergraduate experience at the Krieger School allows students to build basic abilities that allow them to flourish and learn throughout their life.</p>
-				<div class="my-4"><a href="<?php echo esc_url( site_url( '/undergraduate-experience/' ) ); ?>" class=" bg-blue text-white font-heavy font-bold text-base lg:text-lg p-4 hover:bg-blue-light hover:text-primary rounded-md border-none">Explore Undergraduate Education</a></div>
+				<p class="text-2xl py-4">The Krieger School of Arts & Sciences sets the standard for excellence in research-based undergraduate education.</p>
+				<div class="my-4"><a href="https://undergrad.krieger.jhu.edu/" class=" bg-blue text-white font-heavy font-bold text-base lg:text-lg p-4 hover:bg-blue-light hover:text-primary rounded-md border-none">Explore Undergraduate Education</a></div>
 			</div>
-			<figure class="wp-block-media-text__media not-prose"><img src="http://krieger.jhu.edu/wp-content/uploads/2021/08/CIIP_impact.jpg" alt="three women in a garden with large hats" class="wp-image-15056 size-full"/></figure>
+			<figure class="wp-block-media-text__media not-prose"><img src="https://krieger.jhu.edu/wp-content/uploads/2024/10/FirstYrSeminarFall_JHU4055.jpg" alt="Students sitting on the quad, gathered around a large checkerboard playing a game of checkers" class="size-full"/></figure>
 		</div>
 	</div>
 </section>
